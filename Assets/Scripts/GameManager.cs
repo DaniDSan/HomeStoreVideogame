6441468aab93e4f08399a0f04d0f760106492093
CrovettoDEV <crovettodevcontacto@gmail.com>
2025-02-05 01:19:21 +0100

    GameManagerChanges

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,17 +2,79 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum ScreenName
+{
+    MainMenu,Pause,Options
+}
+
+[System.Serializable]
+public struct Screen
+{
+    //Id con el que controlamos el tipo de pantalla.
+    public ScreenName name;
+
+    //Objeto que actua como esa pantalla.
+    public GameObject reference;
+}
+
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("General")]
+    public bool onApartment;
+
+    [Header("Dinero")]
+    [SerializeField] private float currentMoney;
+
+    [Header("Screens")]
+    [SerializeField] private List<Screen> screens;
+
+    [SerializeField] private Screen currentScreen;
+
+    [SerializeField] private ScreenName lastScreen;
+
+    public static GameManager instance;
+
+    private void Awake()
     {
-        
+        if(instance == null)
+        {
+            instance = this;
+        }
+
+        //Hacemos que por defecto se almacene en la currentScreen que este indicada en el enum.
+        foreach (Screen screen in screens)
+        {
+            if (screen.name == currentScreen.name)
+            {
+                currentScreen.reference = screen.reference;
+                return;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ChangeScreen(ScreenName nextScreenName)
     {
-        
+        foreach (Screen screen in screens)
+        {
+            if(screen.name == nextScreenName)
+            {
+                Debug.Log("Cambiando a pantalla " + nextScreenName.ToString());
+
+                //1.Activamos la nueva pantalla.
+                screen.reference.SetActive(true);
+
+                //2.Desactivamos la pantalla actual en la que estaba el jugador.
+                currentScreen.reference.SetActive(false);
+
+                //3.Indicamos cual es la nueva LastScreen.
+                lastScreen = currentScreen.name;
+
+                //4.Actualizamos cual es la currentScreen.
+                currentScreen = screen;
+
+                //5.Salimos de la función porque se ha completado el cambio de pantalla.
+                return;
+            }
+        }
     }
 }
