7b753b49d86aa21cf0c6212190e53fba1b8b90b5
CrovettoDEV <crovettodevcontacto@gmail.com>
2025-03-07 00:32:41 +0100

    UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public enum ScreenName
 {
-    MainMenu,Pause,Options
+    MainMenu,Pause,Options,CharacterSelector,World
 }
 
 [System.Serializable]
@@ -17,10 +20,20 @@ public struct Screen
     public GameObject reference;
 }
 
+[System.Serializable]
+public struct Character
+{
+    public string name;
+
+    public Sprite sprite;
+}
+
 public class GameManager : MonoBehaviour
 {
     [Header("Dinero")]
     [SerializeField] private float currentMoney;
+
+    [SerializeField] private TextMeshProUGUI moneyText;
 
     [Header("Screens")]
     [SerializeField] private List<Screen> screens;
@@ -28,6 +41,14 @@ public class GameManager : MonoBehaviour
     [SerializeField] private Screen currentScreen;
 
     [SerializeField] private ScreenName lastScreen;
+
+    [Header("Personajes")]
+    [SerializeField] private Character[] characters;
+
+    [Header("CharacterUI")]
+    [SerializeField] private Image playerImage;
+
+    [SerializeField] private TextMeshProUGUI playerName;
 
     public static GameManager instance;
 
@@ -46,6 +67,15 @@ public class GameManager : MonoBehaviour
                 currentScreen.reference = screen.reference;
                 return;
             }
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (currentScreen.name == ScreenName.World) ChangeScreen(ScreenName.Pause);
+            else if (currentScreen.name == ScreenName.Pause) ChangeScreen(ScreenName.World);
         }
     }
 
@@ -73,5 +103,86 @@ public class GameManager : MonoBehaviour
                 return;
             }
         }
+    }
+
+    public void SetCharacter(Sprite sprite)
+    {
+        foreach (Character character in characters)
+        {
+            if(sprite == character.sprite)
+            {
+                Debug.Log("Se ha elegido un personaje");
+
+                playerImage.sprite = character.sprite;
+
+                playerName.text = character.name;
+
+                break;
+            }
+        }
+        ChangeScreen(ScreenName.World);
+    }
+
+    public void ChangeScreen(string nextScreenName)
+    {
+        foreach (Screen screen in screens)
+        {
+            if(screen.name.ToString() == nextScreenName)
+            {
+                ChangeScreen(screen.name);
+
+                return;
+            }
+        }
+        Debug.LogError("Se esta intentando acceder a una pantalla que no existe.");
+    }
+
+    public void PauseGame(bool state)
+    {
+        if (state)
+        {
+            Time.timeScale = 0f;
+
+            ChangeScreen(ScreenName.Pause);
+        }
+        else
+        {
+            Time.timeScale = 1f;
+
+            ChangeScreen(ScreenName.World);
+        }
+    }
+
+    public void ReturnMainMenu()
+    {
+        Time.timeScale = 1f;
+
+        SceneManager.LoadScene(0);
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Saliendo del videojuego");
+
+        Application.Quit();
+    }
+
+    public void AddMoney(int amount)
+    {
+        currentMoney += amount;
+
+        UpdateMoneyTexts();
+    }
+
+    public void RemoveMoney(int amount)
+    {
+        currentMoney -= amount;
+
+        UpdateMoneyTexts();
+    }
+
+    private void UpdateMoneyTexts()
+    {
+        moneyText.text = currentMoney.ToString("N0");
     }
 }
